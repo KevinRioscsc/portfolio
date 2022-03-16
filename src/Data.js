ca8361c95df97c23500d048b82762710b37cf045
Kevin Rios <rioskevin89@gmail.com>
2022-03-16 12:52:54 -0700

    more projects

--- src/Data.js
+++ src/Data.js
@@ -1,12 +1,25 @@
-import project from './Components/Images/smart-brain.png'
+import Funk from './Components/Images/theFunk.png'
 import Frend from './Components/Images/Frend2.png'
 import Genie from './Components/Images/Genie.png'
 import Kimo from './Components/Images/Kimo.png'
-import Amazon from './Components/Images/Amazon.png'
+import Quest from './Components/Images/quest2.png'
 import Chain from './Components/Images/Chain.png'
 
 export const projectAll = [
-
+{
+    projectName: 'Quest',
+    projectDesc : "I created this app with the intentions of cretaing a workspace board for developers to track their own progress on their own projects or with other miscellenious things like creating a PC. Quest lets you create a quest and with that quest you can add any amount of activities that'll help you complete your own quest. It serves as a way to keep track of your own progress.",
+    projectImg : Quest,
+    projectLive: 'https://rapidapi.com/rioskevin89@gmail.com/api/amazon-data-scraper56/',
+    projectSource: 'https://github.com/KevinRioscsc/Amazon-Scraper'
+},
+{
+    projectName: 'Kimo Wireless',
+    projectDesc : "A landing site for a relatively new technology repair shop. I made sure that the client's request was met in every aspect. The client was satisfied and it made an immediate impact to their sales as the website showcases the skill of the repair man. ",
+    projectImg : Kimo,
+    projectLive: 'https://kimowireless.com',
+    projectSource: 'https://github.com/KevinRioscsc/Kimo-Wireless'
+},
 {
     projectName: 'Frend',
     projectDesc : "Frend is a social media music sharing app that uses the spotify API to search for your favorite songs to post on the global newsfeed. Everybody could see what songs you recommend along with a little description of what the song makes you feel. The Login and Register feature uses Firebase Auth and post and likes are stored in a postgres database",
@@ -17,25 +30,11 @@ export const projectAll = [
 },
 {
     projectName:'Genie',
-    projectDesc : "Blockchain has all the information you need to become a cryptocurrency guru. Blockchain gives you every bit of information of every cryptocurrency in the world. Using the CryptoCurrency API BlockChain allows you to see every bit of information, and current real time news of every cryptocurrency and exchange",
+    projectDesc : "Genie, the spawn of Google, is a search engine that gives all the information in the world with a touch of button. Links, News, and Images of everything you want to search up. Using the Google API from RAPID API i was able to make a Google clone using the JSON data given from the API.",
     projectImg : Genie,
-    projectLive: 'https://kevinrioscsc.github.io/FunkyBots/',
-    projectSource: 'https://github.com/KevinRioscsc/FunkyBots'
+    projectLive: 'genie-search.herokuapp.com',
+    projectSource: 'https://github.com/KevinRioscsc/Genie-Search-Engine'
 
-},
-{
-    projectName: 'Kimo Wireless',
-    projectDesc : "A landing site for a relatively new technology repair shop. I made sure that the client's request was met in every aspect. The client was satisfied and it made an immediate impact to their sales as the website showcases the skill of the repair man. ",
-    projectImg : Kimo,
-    projectLive: 'https://kevinrioscsc.github.io/Kimo-Wireless/',
-    projectSource: 'https://github.com/KevinRioscsc/Kimo-Wireless'
-},
-{
-    projectName: 'Amazon Scraper API',
-    projectDesc : "The Amazon Scraper API scrapes real-time data from Amazon to give us endpoints that give us search results, preoduct reviews, and product offers. Using the Scaper API my Amazon Scraper is the best way to recieve this information in a JSON Format",
-    projectImg : Amazon,
-    projectLive: 'https://rapidapi.com/rioskevin89@gmail.com/api/amazon-data-scraper56/',
-    projectSource: 'https://github.com/KevinRioscsc/Amazon-Scraper'
 },
 {
     projectName: 'BlockChain',
@@ -45,11 +44,11 @@ export const projectAll = [
     projectSource: 'https://github.com/KevinRioscsc/Crypto-Info-App'
 },
 {
-    projectName: 'Smart Brain',
-    projectDesc : "SmartBrain is an app that combines React, Node.js, Express, PostgreSQL, and the Clarifai API to create an awesome face recognition app! You could upload a photo to the app and it'll detect a face on the photo. The app tracks the amount of time user has uploaded and used the app. ",
-    projectImg : project,
-    projectLive: 'https://smart-brain-kevin.herokuapp.com',
-    projectSource: 'https://github.com/KevinRioscsc/smart-brain-frontend'
+    projectName: 'Funky Monkey',
+    projectDesc : "I created a Landing Site for my discord bot that I created with the Discord JS npm package. Using heroku I can host the discord bot, so I dont have to keep my computer running 24/7. This site has the link for that bot and lists of commands to help the user navigate the bot.",
+    projectImg : Funk,
+    projectLive: 'https://pedantic-mirzakhani-fa1507.netlify.app',
+    projectSource: 'https://github.com/KevinRioscsc/funky_monkey_site'
 
 }
 ]
